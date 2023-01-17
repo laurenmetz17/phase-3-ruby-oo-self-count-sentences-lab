01180b9cd568e5e8a00c460f6aef075772f9ea47
Lauren Metz <Laurenmetz15@gmail.com>
2023-01-16 22:39:04 -0500

    commit tree c2a623f16da6d8f7d8465cc2dc452f0a8332348f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    last = self.chars.last
+    last == "."
   end
 
   def question?
-
+    last = self.chars.last
+    last == "?"
   end
 
   def exclamation?
-
+    last = self.chars.last
+    last == "!"
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences = sentences.filter do |sentence|
+      sentence !=  ""
+    end
+    sentences.length
   end
 end
